62f7f02a5eaba50b680455ff1330896701378f67
Nikhil Arora <narora200@gmail.com>
2017-08-12 23:51:40 +0530

    script added for nodemon in package.json

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 
-app.listen(3333, () => {
-	console.log("Listening on port 3333");
+app.listen(3334, () => {
+	console.log("Listening on port 3334");
 });
