22c8dd853c40310b6fe1d4e7e2742c953e8c6329
Nikhil Arora <narora200@gmail.com>
2017-08-12 23:55:54 +0530

    route set for /graphql

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const expressGraphQL = require('express-graphql');
 
 const app = express();
 
+app.use('/graphql', expressGraphQL({
+	schema: schema,
+	graphiql: true
+}))
 
 app.listen(3334, () => {
 	console.log("Listening on port 3334");
