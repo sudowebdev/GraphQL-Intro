56006f8b7b160c013696d38460ff0df70a10b7a7
Nikhil Arora <narora200@gmail.com>
2017-08-13 08:35:29 +0530

    Added the hardcoded data

--- schema.js
+++ schema.js
@@ -7,6 +7,15 @@ const {
 	GraphQLNonNull
 } = require('graphql');
 
+// Hardcoded data
+const customers = [
+	{id: '1', name: "Nikhil Arora", email: 'nikhil@gmail.com', age: 20},
+	{id: '2', name: "Saumya Tiwari", email: 'saumya@gmail.com', age: 21},
+	{id: '3', name: "Dalip Thakkar", email: 'dalip@gmail.com', age: 20}
+];
+
+
+
 // Customer Type
 const CustomerType= new GraphQLOBjectType({
 	name: 'Customer',
