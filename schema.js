05352fb60a8bc76d81db11fab5a2e91af095cf06
Nikhil Arora <narora200@gmail.com>
2017-08-13 08:41:06 +0530

    Defined the RootQuery

--- schema.js
+++ schema.js
@@ -33,7 +33,17 @@ const RootQuery = new GraphQLObjectType({
 	name: 'RootQueryType',
 	fields: {
 		customer: {
-			type: CustomerType
+			type: CustomerType,
+			args: {
+				id: {type: GraphQLString}
+			},
+			resolve(parentValue, args) {
+				for(let i=0; i<customers.length; i++){
+					if(customers[i].id == args.id){
+						return customers[i];
+					}
+				}
+			}
 		}
 	}
 })
