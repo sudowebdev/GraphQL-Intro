1ecc4bbc3d6223490ee5d495b3797e95d175bd29
Nikhil Arora <narora200@gmail.com>
2017-08-13 08:31:34 +0530

    CustomerType defined

--- schema.js
+++ schema.js
@@ -7,6 +7,27 @@ const {
 	GraphQLNonNull
 } = require('graphql');
 
+// Customer Type
+const CustomerType= new GraphQLOBjectType({
+	name: 'Customer',
+	fields: () => ({
+		id: {type: GraphQLString},
+		name: {type: GraphQLString},
+		email: {type: GraphQLString},
+		age: {type: GraphQLInt}
+	})
+})
+
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		customer: {
+			type: CustomerType
+		}
+	}
+})
 
 
 module.exports = new GraphQLSchema({
